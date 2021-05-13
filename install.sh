a34792702592d36cf4e5111c0cc80cb876445469
frantisekfarkas <frantisekfarkas93@gmail.com>
2021-05-13 13:30:24 +0200

    Install script adjustments

--- install.sh
+++ install.sh
@@ -1,20 +1,43 @@
+#!/bin/sh
+
+#Defining variable for launcher
+launcher=/home/pi/zeroconf.api.service/launcher.sh
+logFile=/home/pi/logs/cronlog
+
+
+#Testing if logs folder exists
+if [ -e "/home/pi/logs" ]
+then
+	echo "Folder logs already exists"
+else
+	echo "Creating logs folder.."
+	mkdir /home/pi/logs
+    cd /home/pi/
+fi
+
+
+#Adding Zeroconf API to crontab
 add_cronjob () { 
     echo "Adding Zeroconf API as a cronjob"
     crontab -l > newcron
-    echo "@reboot sh /home/pi/zeroconf.api.service/launcher.sh >/home/pi/logs/cronlog 2>&1" >> file
+    echo "@reboot sh /home/pi/zeroconf.api.service/launcher.sh > $logFile 2>&1" >> newcron
     crontab newcron
     rm -f newcron
 }
 
+  
+crontab -l | grep "$launcher"
+if [ $? -eq 0 ]
+	then
+	    echo "Job already added to crontab"
+    else
+	    echo "Adding job to crontab..."
+	    add_cronjob
+fi
 
-if [[ -d "/home/pi/logs" ]];
- then
-    echo "logs folder already exists"
-    add_cronjob()
-    
-else 
-    mkdir /home/pi/logs
-    cd /home/pi/
-    add_cronjob()
-fi 
-echo "please restart your raspberry pi to start the ZeroConfAPI"
+if [ -e $logFile ]
+then
+	echo "File '$logFile' is already created"
+else
+	echo "Please restart your raspberry pi to start the ZeroConfAPI"
+fi
