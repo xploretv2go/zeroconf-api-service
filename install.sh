8299eaa12f3ed96bd1b740854c47155f7ded0071
frantisekfarkas <frantisekfarkas93@gmail.com>
2021-06-23 22:48:25 +0200

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
 #Defining variable for launcher
-launcher=/home/pi/zeroconf.api.service/launcher.sh
-logFile=/home/pi/zeroconf.api.service/logs/cronlog
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+launcher="${parent_path}/launcher.sh"
+logFile="${parent_path}/logs/cronlog"
+
+#Testing if logs folder exists
+if [ -e "${parent_path}/logs" ]
+then
+	echo "Folder logs already exists"
+else
+	echo "Creating logs folder.."
+	mkdir ${parent_path}/logs
+    cd /
+fi
+
 
 
 #Adding Zeroconf API to crontab
 add_cronjob () { 
     echo "Adding Zeroconf API as a cronjob"
     crontab -l > newcron
-    echo "@reboot sh /home/pi/zeroconf.api.service/launcher.sh > $logFile 2>&1" >> newcron
+    echo "@reboot sh ${launcher} > $logFile 2>&1" >> newcron
     crontab newcron
     rm -f newcron
 }
--- launcher.sh
+++ launcher.sh
@@ -2,8 +2,7 @@
 #launcher.sh
 
 
-cd /
-cd home/pi/zeroconf.api.service
+cd "$(dirname "$0")"
 sudo pip3 install -r requirements.txt
 sudo python3 run.py
 cd /
