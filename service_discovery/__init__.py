648a8a33ebdcd814b783d016d94c943ec27ae523
frantisekfarkas <frantisekfarkas93@gmail.com>
2021-06-27 22:17:21 +0200

    Added ipv6 address for services registered

--- service_discovery/__init__.py
+++ service_discovery/__init__.py
@@ -22,6 +22,7 @@ from zeroconf import (
 load_dotenv()
 iface = netifaces.gateways()["default"][netifaces.AF_INET][1]
 ip_address = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]["addr"]
+ipv6_address = netifaces.ifaddresses(iface)[netifaces.AF_INET6][0]["addr"]
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -144,12 +145,14 @@ def serviceToOutput(info):
 def selfRegister():
     props = {"get": "/a1/xploretv/v1/zeroconf"}
 
+    print(ipv6_address)
+
     service = ServiceInfo(
         "_http._tcp.local.",
         "ZeroConf Service Discovery API at "
         + socket.gethostname()
         + "._http._tcp.local.",
-        addresses=[socket.inet_aton(ip_address)],
+        addresses=[socket.inet_aton(ip_address), socket.inet_aton(ip_address)],
         port=int(os.getenv("PORT")),
         properties=props,
         server=str(socket.gethostname() + "."),
@@ -309,7 +312,10 @@ class ServicesRoute(Resource):
             new_service = ServiceInfo(
                 parsedType,
                 wildcard_name,
-                addresses=[socket.inet_aton(ip_address)],
+                addresses=[
+                    socket.inet_aton(ip_address),
+                    socket.inet_aton(ipv6_address),
+                ],
                 port=args.service["port"],
                 server=str(socket.gethostname() + "."),
                 properties=args.service["txtRecord"],
