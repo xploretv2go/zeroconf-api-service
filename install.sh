629e8cde247d27e0ae22b7292523e0d3559231b7
frantisekfarkas <frantisekfarkas93@gmail.com>
2021-07-13 22:23:50 +0200

    Changed install.sh script to add hosts alias

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;}
+
 #Defining variable for launcher
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
@@ -15,6 +17,28 @@ else
 	mkdir ${parent_path}/logs
     cd /
 fi
+
+echo "Modifying /etc/hosts file"
+
+
+if [[ $(sed -n '/^127.0.0.1/p' /etc/hosts) == *"localzeroconf"* ]]; then
+	echo "IPv4 Host already modified"
+else
+	sed -i '/^127.0.0.1/ s/$/ localzeroconf/' /etc/hosts
+	echo "IPv4 Host successfully modified"
+fi
+
+
+
+if [[ $(sed -n '/^::1/p' /etc/hosts) == *"ip6-localzeroconf"* ]]; then
+	echo "IPv6 Host already modified"
+else
+	sed -i '/^::1/ s/$/ ip6-localzeroconf/' /etc/hosts
+	echo "IPv6 Host successfully modified"
+fi
+
+echo "Localzeroconf alias set!"
+
 
 
 
